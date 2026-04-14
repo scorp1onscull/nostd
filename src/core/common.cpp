bd010a35c2f996ecdc5521bbecc7d548e5a1dea9
st142661 <st142661@student.spbu.ru>
2026-04-14 12:56:42 +0300

    feat: add core cs and math

---
+++ src/core/common.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+void credits() {
+    std::cout << "libnostd v.0.0.1 - A library with somewhat useful functions\n";
+    std::cout << "Licensed by MIT License\n";
+    std::cout << "Developed&Maintained by @scorp1onscull\n";
+    std::cout << '\n';
+}
